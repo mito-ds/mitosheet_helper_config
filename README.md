37414d44db54dd4b52d1e9a548f33a3ed3b0248e
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2022-10-11 13:45:32 -0400

    export variables properly

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 This package is used to configure the mitosheet, mitosheet2, and mitosheet3 packages. 
 It should only be used with a valid Mito Enterprise liscence. 
+
+# Release to test pypi
+Delete the dist folder, and the .egg-info folder
+Bump the version in setup.cfg and pyproject.toml
+Run `python3 -m build`
+Run `python3 -m twine upload --repository testpypi dist/*`
--- mitosheet_helper_config/mito_config.py
+++ mitosheet_helper_config/mito_config.py
@@ -1,11 +1,11 @@
 
 # These variables must match the variables defined in 
-# mito_configuration.py of the mitosheet package
+# mito_config.py of the mitosheet package
 MEC_VERSION = 'mec_version'
 SUPPORT_EMAIL = 'support_email'
 
 
 MITO_ENTERPRISE_CONFIGURATION = {
-		MEC_VERSION: 1,
-		SUPPORT_EMAIL: 'mito-support@company.com'
+	MEC_VERSION: 1,
+	SUPPORT_EMAIL: 'mito-support@company.com'
 }
