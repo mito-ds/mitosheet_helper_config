fa828094e3fa6071e6f92f3235099a3aa3ceaf1d
Aaron Diamond-Reivich <aarondia@wharton.upenn.edu>
2022-10-20 13:16:39 -0400

    add instructions for using the mitosheet_helper_config package

--- mitosheet_helper_config/mito_config.py
+++ mitosheet_helper_config/mito_config.py
@@ -1,11 +1,21 @@
+from mitosheet_helper_config.mito_config_utils import *
 
-# These variables must match the variables defined in 
-# mito_config.py of the mitosheet package
-MEC_VERSION = 'mec_version'
-SUPPORT_EMAIL = 'support_email'
+"""
+This file contains all of the Mito configuration options for Enterprise admins.
 
+To configure your Mito deployment, follow the instructions above each of the entries
+in the MITO_ENTERPRISE_CONFIGURATION dictionary below.
+"""
 
 MITO_ENTERPRISE_CONFIGURATION = {
+	# The MEC_VERSION is used internally by Mito to properly read the 
+	# MITO_ENTERPRISE_CONFIGURATION dictionary. Do not edit.
 	MEC_VERSION: 1,
-	SUPPORT_EMAIL: 'mito-support@company.com'
+
+	# Users get directed to the SUPPORT_EMAIL when an error occurs or they seek help. 
+	# Update the SUPPORT_EMAIL with a valid email address. If the SUPPORT_EMAIL is None, 
+	# users will get directed to the Mito community slack. The final result should look like:
+	# 	
+	# 	SUPPORT_EMAIL: 'mito_support@company.com',
+	SUPPORT_EMAIL: None, 
 }
